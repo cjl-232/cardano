d568fb1dce422b77122b59baba90c6c084e202ba
Chris Linnell <chrislinnell232@gmail.com>
2025-08-20 14:39:27 +0100

    Added categories with recursion

--- skills/models.py
+++ skills/models.py
@@ -1,10 +1,49 @@
+from typing import Any
+
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+class Category(models.Model):
+    id: models.BigAutoField
+    name = models.CharField(max_length=255)
+    parent: models.ForeignKey['Category | None'] = models.ForeignKey(
+        to='self',
+        on_delete=models.PROTECT,
+        related_name='children',
+        blank=True,
+        null=True,
+    )
+
+    def clean(self):
+        ancestor = self.parent
+        while ancestor is not None:
+            if ancestor.id == self.id:
+                raise ValidationError('Loop detected in parent chain.')
+            ancestor = ancestor.parent
+
+    def save(self, *args: Any, **kwargs: Any):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        db_table = 'skills_categories'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'parent'],
+                name='skills_categories_AK01',
+            ),
+        ]
+
+
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    category = models.ForeignKey(
+        to=Category,
+        on_delete=models.PROTECT,
+    )
 
     class Meta:
         db_table = 'skills_skills'
@@ -38,7 +77,7 @@ class SkillEntry(models.Model):
             models.UniqueConstraint(
                 fields=['user', 'skill'],
                 name='skills_skill_entries_AK01',
-            )
+            ),
         ]
 
 # Create your models here.
