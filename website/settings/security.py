bdbf9e822896e4b8b19c60db0b5e20ffbfc68472
Chris Linnell <chrislinnell232@gmail.com>
2025-08-24 09:33:38 +0100

    Adjusted operation order

--- website/settings/security.py
+++ website/settings/security.py
@@ -2,6 +2,7 @@ import os
 
 from dotenv import load_dotenv
 
-# The secret key must be loaded from the environment or a .env file.
 load_dotenv(override=False)
+
+# The secret key must be loaded from the environment or a .env file.
 SECRET_KEY = os.environ['SECRET_KEY']
